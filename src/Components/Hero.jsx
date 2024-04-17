a94d98034e83f3be5bfb016fac4e250f53806179
MrChocolate <mrchocolatepr@gmail.com>
2024-04-16 21:22:09 -0400

    Agregando Componentes de Rutas

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,19 @@
+const Hero = () => {
+
+    const heroStyles = {
+
+    }
+  return (
+    <div style={heroStyles}>
+        <h1>
+            Curiana Radio
+        </h1>
+
+        <img src="" alt="" />
+
+
+    </div>
+  )
+}
+
+export default Hero
